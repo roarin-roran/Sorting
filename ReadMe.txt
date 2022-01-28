c9ccbaebf997f88f99eb37c1b38c0ffbe05c71f8
Ben <benjaminmarksmith@outlook.com>
2022-01-28 20:57:15 +0000

    k way merging added
    with a simple scan as the method for finding the minimum

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,6 @@
 sorterManager creats inputs with various levels of presortedness, and will be expanded to add analysis functions
 
-mergeSorter sorts an input using two-way ping-pong merge sort, and will be expanded to add 3/4/k-way merging and run detection.
+mergeSorter sorts an input using two-way ping-pong merge sort, as well as a k-way merge sort
+it will be expanded to add run detection.
 
 the mothballed folder contains several old attempts at making the mergeSorter class, retained for reference.
--- mergeSorter.py
+++ mergeSorter.py
@@ -1,3 +1,6 @@
+# todo:
+# 1. find the next element for k-way merging via an optimal method.
+
 import math
 
 class mergeSorter:
@@ -34,11 +37,13 @@ class mergeSorter:
 	# * * * * * MERGERS * * * * *
 	# * * * * * * * * * * * * * *
 
-	# merges two runs. runs must be adjacent to maintain stability, so are indicated by start, middle and end
-	# start points are always inclusive, and end points are always exclusive so it is legit to use mid for both,
-	# without causing overlap 
+	# merges two runs. runs must be adjacent to maintain stability, so
+	# are indicated by start, middle and end start points are always
+	# inclusive, and end points are always exclusive so it is legit to
+	# use mid for both, without causing overlap
 	def merge2(self, start, mid, end):
-		# the left run and modified section of the working our list start at start of the relevent section
+		# the left run and modified section of the working our list
+		# start at start of the relevent section
 		leftPos = start
 		self.workingOutPos = start
 		# the midpoint is defined as the start of the right hand run
@@ -49,7 +54,8 @@ class mergeSorter:
 
 		# while you're still inside both runs
 		while leftPos < mid and rightPos < end:
-			# if the next LH element is smaller than the next RH element
+			# if the next LH element is smaller than the next RH
+			# element
 			if currentList[leftPos] < currentList[rightPos]:
 				# use it
 				self.sendToOutput(workingOutList, currentList[leftPos])
@@ -67,6 +73,54 @@ class mergeSorter:
 			self.sendToOutput(workingOutList, currentList[rightPos])
 			rightPos += 1
 
+
+	# merges k runs, where k is the number of lists contained within
+	# the runs list.
+	#
+	# the runs list should contain [start, end] pairs, where the start
+	# is inclusive and the end is exclusive.
+	#
+	# runs are assumed to be contiguous (necesary for stability), so
+	# at input the  start of each run should be the end of the
+	# previous run, though this will be modified as elements are moved
+	# to output
+	def mergeK(self, runs):
+		# overwritten portion of workingOutPos starts at the start of
+		# the set of runs
+		self.workingOutPos = runs[0][0]
+
+		currentList = self.getCurrentList()
+		workingOutList = self.getWorkingOutList()
+
+		# while there are still runs
+		while len(runs) > 1:
+			# find the run with the smallest first index
+
+			# initialise to the first run
+			minRun = 0
+			for i in range(len(runs) - 1):
+				# if the first element is smaller, update it.
+				if currentList[runs[i+1][0]] < currentList[runs[minRun][0]]:
+					minRun = i + 1
+
+			# move that element to the output
+			self.sendToOutput(workingOutList,currentList[runs[minRun][0]])
+
+			# increment position in this run
+			runs[minRun][0] += 1
+
+			# if the current and end pos of that run are now equal,
+			# remove it from the list
+			if runs[minRun][0] == runs[minRun][1]:
+				runs.pop(minRun)
+
+		# copy remaining run over - no need to pay the overheads of comparing 
+		while runs[0][0] != runs[0][1]:
+			self.sendToOutput(workingOutList,currentList[runs[0][0]])
+			runs[0][0] += 1
+
+
+
 	# * * * * * * * * * * * * * *
 	# * * * * * SORTERS * * * * *
 	# * * * * * * * * * * * * * *
@@ -76,6 +130,7 @@ class mergeSorter:
 		runLength = 1
 
 		while runLength <= self.inputLength:
+			# for all runs
 			for i in range(math.ceil(self.inputLength/(2*runLength))):
 				# beginning of a run is assumed to be safe
 				start = runLength*2*i
@@ -100,15 +155,60 @@ class mergeSorter:
 		return self.getCurrentList()
 
 
+	# merge sort, merging k runs at a time
+	def mergeSortK(self, k):
+		runLength = 1
+
+		# while some merges remain
+		while runLength <= self.inputLength:
+
+			# for each block of k runs
+			for blockNumber in range(math.ceil(self.inputLength/(k*runLength))):
+				
+				start = runLength*k*blockNumber
+				
+				# an array in the format [[start,end],[start,end]...]
+				mergePlan = []
+
+				# for each run in the block - fill the merge plan
+				for runNumber in range(k):
+					firstElement = runLength*k*blockNumber + runNumber*runLength
+					
+					# if the first element is over the end of the
+					# array, this mergeplan is complete
+					if firstElement >= self.inputLength:
+						break
+					
+					lastElement = firstElement + runLength
+					# if the last element overruns, truncate the run
+					# to include whatever's left 
+					if lastElement > self.inputLength:
+						lastElement = self.inputLength
+
+					mergePlan.append([firstElement,lastElement])
+
+				self.mergeK(mergePlan)
+
+			# swap between the ping list and pong list after all operations are complete
+			self.pingCurrent = not self.pingCurrent
+
+			runLength *= k
+
+
 ourInput = [17,16,13,12,9,3,12,34,65,12,34,76,8,12,31,4]
+#ourInput = [16,17,12,13,3,9,12,34,12,65,34,76,8,12,4,31]
 ourMS = mergeSorter(ourInput)
 
 print("before:")
 print(ourMS.getCurrentList())
 
-ourMS.mergeSort2()
+#ourMS.mergeSort2()
+#ourMS.mergeK([[0,2],[2,4],[4,6]])
+
+ourMS.mergeSortK(3)
 
 print()
 print("after:")
 print(ourMS.getCurrentList())
+#print(ourMS.getWorkingOutList())
 
