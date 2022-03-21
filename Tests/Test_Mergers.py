3347b19785315b4196df0bbb1867349d9a10f1ea
Ben <benjaminmarksmith@outlook.com>
2022-03-21 19:10:11 +0000

    merger testing fixed
    before, adding new mergers would require reqriting the testing program.
    
    there's now one test per object in test_mergers, and everything else is a sorter method

--- Tests/Test_Mergers.py
+++ Tests/Test_Mergers.py
@@ -6,20 +6,19 @@ from Mergers import Merger_Adaptive
 
 class Test_Mergers(unittest.TestCase):
 
-    def test_merge_two(self):
+    def merge_two(self, merger_init):
         two_runs = [2, 3, 0, 1]
         run_1 = ListSlice.ListSlice(two_runs, 0, 2)
         run_2 = ListSlice.ListSlice(two_runs, 2, 4)
 
         write_list_slice = ListSlice.ListSlice([-1, -1, -1, -1], 0, 4)
 
-        # fiddle with this when more mergers are added
-        our_merger = Merger_Adaptive.Merger_Adaptive([run_1, run_2], write_list_slice)
+        our_merger = merger_init([run_1, run_2], write_list_slice)
         our_merger.merge()
 
         self.assertEqual(write_list_slice.list, sorted(write_list_slice.list))
 
-    def test_merge_three_variable_lengths(self):
+    def merge_three_variable_lengths(self, merger_init):
         three_runs = [7, 0, 2, 4, 1, 3, 5, 6]
         run_1 = ListSlice.ListSlice(three_runs, 0, 1)
         run_2 = ListSlice.ListSlice(three_runs, 1, 4)
@@ -27,8 +26,7 @@ class Test_Mergers(unittest.TestCase):
 
         write_list_slice = ListSlice.ListSlice([-1] * 7, 0, 7)
 
-        # fiddle with this when more mergers are added
-        our_merger = Merger_Adaptive.Merger_Adaptive([run_1, run_2, run_3], write_list_slice)
+        our_merger = merger_init([run_1, run_2, run_3], write_list_slice)
         our_merger.merge()
 
         self.assertEqual(write_list_slice.list, sorted(write_list_slice.list))
@@ -36,6 +34,10 @@ class Test_Mergers(unittest.TestCase):
     def test_adaptive_sort(self):
         sorter_tester = Test_Sorters.Test_Sorters()
         sorter_tester.test_sorter_bottom_up(merger_init=Merger_Adaptive.Merger_Adaptive)
+
+        self.merge_two(Merger_Adaptive.Merger_Adaptive)
+
+        self.merge_three_variable_lengths(Merger_Adaptive.Merger_Adaptive)
 
 
 if __name__ == '__main__':
