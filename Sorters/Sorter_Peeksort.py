356c621350a48cbdbb15f9ea133ee7b047e3319d
Ben <benjaminmarksmith@outlook.com>
2022-04-18 10:42:54 +0100

    initial prototype of k-way peeksort
    in a verbose form, with dummy methods for recursion past the first layer, merge stack addition, and merging.
    
    memory is non-final, but not bad for a prototype - anywhere that lists are sliced, expect something better in the final version

---
+++ Sorters/Sorter_Peeksort.py
@@ -0,0 +1,196 @@
+from Sorters import Sorter
+from Support import ListSlice
+import random
+
+
+class Sorter_Peeksort(Sorter.Sorter):
+    def __init__(self, input_list, k,
+                 merger_ipq_init=False,
+                 merger_init=False,
+                 test_mode=False):
+        super().__init__(input_list, k, merger_ipq_init, merger_init, test_mode)
+
+    def sort(self):
+        """put the list in sorted order using k way peeksort"""
+
+        # todo - detect the first and last run
+        self._recursively_sort(0, 0, len(self.input_list), len(self.input_list))
+
+    # sorts the input
+    def _recursively_sort(self, input_start, first_run_end, last_run_start, input_end):
+        """recursively sort an input assumed to contain at least two runs"""
+        # 1. test for and handle inputs with at most k inputs that aren't in known runs.
+        #   (note that inputs where the first run overlaps the second are illegal)
+        if last_run_start - first_run_end <= self.k:
+            self._handle_trivial_input(input_start, input_end, first_run_end, last_run_start)
+        else:
+            # find all m values outside of a known run, steps 2 and 3
+            m = self._generate_initial_m_values(first_run_end, last_run_start)
+
+            # 6. initialise the dangling end
+            next_input_start = input_start
+            next_first_run_end = first_run_end
+
+            # 4. generate recursive calls and merges using these m values
+            print("\nm:", m, "\n")
+
+            i = 0
+            while i < len(m):
+                print("\tm_i:       ", m[i])
+
+                # 5. find the left edge of the run (extend run left)
+                l_i = self._extend_run_left(m[i], next_first_run_end)
+                print("\tl_i:       ", l_i)
+
+                # note that step 6 has been moved, and a rewrite is required because this is kinda confusing.
+
+                # 7. find the rightmost edge of the run (extend run right)
+                r_i = self._extend_run_right(m[i], last_run_start)
+                print("\tr_i:       ", r_i)
+
+                # todo - do this with a function that returns the value to add to i for neatness
+                # 8. if any other m_i values are in this run: discard them, recurse on the dangly bit, merge the run
+                skip_runs = 0
+                # for all m values after the current one.
+                for j in range(i+1, len(m)):
+                    if m[j] < r_i:
+                        print("\tSKIP element:", j + 1)
+                        print("\tcurrent element:", i)
+                        print("\tcurrent m:", m)
+                        skip_runs += 1
+                    else:
+                        break
+
+                if skip_runs:
+                    # increment i to skip the runs
+                    i += skip_runs
+
+                    # todo - this might not work perfectly in edge cases - test it more thoroughly
+                    # if there's anything to recurse on
+                    if next_input_start < l_i:
+                        # recurse on the dangly bit, and throw it on the merge stack
+                        print("\t\trecurse on:", next_input_start, next_first_run_end, l_i, l_i)
+                        print("\t\tmerge:     ", next_input_start, l_i)
+                    # throw the discovered run on the merge stack
+                    print("\t\tmerge:     ", l_i, r_i)
+                    next_input_start = r_i
+                    next_first_run_end = r_i
+                # 9. if not, it depends on whether m_i is closer to l_i or r_i
+                else:
+                    if m[i] - l_i <= r_i - m[i]:
+                        print("\tl_i is closer(default)")
+                        # recurse on the dangly bit only
+                        print("\t\trecurse on:", next_input_start, next_first_run_end, l_i, l_i)
+                        print("\t\tmerge:     ", next_input_start, l_i)
+
+                        # this run defines the start of the new dangly bit
+                        next_input_start = l_i
+                        next_first_run_end = r_i
+                    else:
+                        print("\tr_i is closer")
+                        # recurse on both the dangly bit and the new run
+                        print("\t\trecurse on:", next_input_start, next_first_run_end, l_i, r_i)
+                        print("\t\tmerge:     ", next_input_start, r_i)
+
+                        # the next dangly bit start without a run.
+                        next_input_start = r_i
+                        next_first_run_end = r_i
+
+                print("\tnext peek time!\n")
+                i += 1
+
+            # 10. handle the last non-run section, and the final run.
+            # if there's a non-run before the last run, recurse on the last section
+            if next_input_start < last_run_start:
+                print("\t\trecurse on:", next_input_start, next_first_run_end, last_run_start, input_end)
+                print("\t\tmerge:     ", next_input_start, input_end)
+            # if there's not, but there is a run - throw it on the merge stack
+            elif last_run_start < input_end:
+                print("\t\tmerge:     ", last_run_start, input_end)
+            # if neither is true, no action is required.
+
+            print("time to merge the merge stack!\n")
+
+        # return when everything that was passed in is a single run
+        return
+
+    def _handle_trivial_input(self, input_start, input_end, first_run_end, last_run_start):
+        # set the first element, while also building a list of the correct length and element type
+        first_run = ListSlice.ListSlice(self.input_list, input_start, first_run_end)
+        runs = [first_run] * (2 + (last_run_start - first_run_end))
+
+        # add middle elements, if any
+        for index in range(first_run_end, last_run_start):
+            runs[index] = ListSlice.ListSlice(self.input_list, index, index + 1)
+
+        if last_run_start != input_end:
+            # add the final run, if it exists
+            runs[-1] = ListSlice.ListSlice(self.input_list, last_run_start, input_end)
+
+        our_merger = self.merger_init(runs, ListSlice.ListSlice(self.input_list, input_start, input_end))
+        our_merger.merge()
+
+    def _generate_initial_m_values(self, first_run_end, last_run_start):
+        # 2. create k-1 evenly spaced m_i values
+        m = [0] * (self.k - 1)
+        step = len(self.input_list) / self.k
+
+        for i in range(self.k - 1):
+            m[i] = round(step * (i + 1))
+
+        # 3. remove any m_i values that are inside the start or end run
+        m_start = 0
+        for m_i in m:
+            if m_i < first_run_end:
+                m_start += 1
+            else:
+                break
+
+        m_end = len(m)
+        for m_i in reversed(m):
+            if m_i > last_run_start:
+                m_end -= 1
+            else:
+                break
+
+        return m[m_start:m_end]
+
+    def _extend_run_left(self, m_i, next_first_run_end):
+        l_i = m_i
+        while l_i > next_first_run_end:
+            if self.input_list[l_i - 1] <= self.input_list[l_i]:
+                l_i -= 1
+            else:
+                break
+
+        return l_i
+
+    def _extend_run_right(self, m_i, last_run_start):
+        r_i = m_i
+        while r_i < last_run_start - 1:
+            if self.input_list[r_i + 1] >= self.input_list[r_i]:
+                r_i += 1
+            else:
+                break
+
+        return r_i
+
+
+# sorts the input
+def sort(input_list, k=2):
+    """creates a sorter object and calls the sort method"""
+    our_peek_sorter = Sorter_Peeksort(input_list, k)
+    our_peek_sorter.sort()
+
+
+input_1 = [4, 3, 2, 1]
+input_2 = [4, 3, 2, 1, 5, 7, 6, 8]
+input_3 = [1, 2, 3, 4, 5, 6, 7, 8]
+input_4 = list(range(20))
+random.shuffle(input_4)
+
+our_input = input_4
+
+print("sorting:", our_input)
+sort(our_input, 5)
+print("sorted: ", our_input)
