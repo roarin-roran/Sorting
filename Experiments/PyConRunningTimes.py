ff9076b143d0a738bdedc7834f2d3eb1166a07f4
Sebastian Wild <wild@liverpool.ac.uk>
2023-04-15 15:16:57 +0100

    add __init__.py to have proper packages

---
+++ Experiments/PyConRunningTimes.py
@@ -0,0 +1,66 @@
+# Running time comparisons between Timsort and Powersort
+# Intended to be run under CPython 3.11 compiled with either Timsort or Powersort
+# as obtained from https://github.com/sebawild/cpython using branch 3.11-instrumented
+#
+# Instructions for compilation:
+# 1. Clone the repository and checkout the branch 3.11-instrumented:
+#    git clone https://github.com/sebawild/cpython.git
+#    git checkout 3.11-instrumented
+# 2. Select the sorting method in Objects/listobject.c (lines 12 onwards):
+#    - Timsort: #define USE_TIMSORT
+#    - Powersort: #define USE_POWERSORT
+#    (At any time, only one of the two must be defined!)
+#    - For running time tests, also remove #define PRINT_INFO
+# 3. Compile CPython with the following command:
+#    ./configure --enable-optimizations
+#    make
+#    make test
+# 4. (optional) Create a venv for the compiled CPython:
+#    ./python -m venv venv
+#    source venv/bin/activate
+#    Check the version of Python:
+#    python --version
+
+#
+#    python3 -m pip install -r requirements.txt
+
+# 5. Run the script:
+#    python PyConRunningTimes.py
+#    (or ./python PyConRunningTimes.py if you are not using a venv)
+#
+
+
+import timeit
+import random
+import Inputs.Inputs as Inputs
+import Inputs.Books as Books
+import Inputs.bad_examples as bad_examples
+
+seed = 2378956239523
+n = 100000
+
+
+RNG = random.Random(seed)
+inputs = [
+    ("random-permutations", lambda: Inputs.random_permutation(n, RNG)),
+    ("random-sqrtn-runs", lambda: Inputs.random_runs(n, int(n ** 0.5), RNG)),
+    ("words-of-bible", lambda: Books.list_of_words_bible()),
+    ("bad-example-cmps", lambda: bad_examples.bad_cmps),
+    ("bad-example-mc", lambda: bad_examples.bad_mc),
+]
+
+# runner = pyperf.Runner()
+# don't use pyperf for now; separate process makes things complicated
+results = []
+for name, input_generator in inputs:
+    lst = input_generator()
+    # Take time using pyperf
+    # time = runner.timeit('sorted(lst)', globals=globals())
+    times = timeit.repeat('sorted(lst)', globals=globals(), number=1, repeat=5)
+    for i, time in enumerate(times):
+        results.append((name, i, time))
+
+# Print results
+for name, i, time in results:
+    # print as table
+    print(f"{name}\t{i}\t{time}")
