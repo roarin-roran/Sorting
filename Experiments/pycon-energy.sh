719fda53cfe98af92f82b6cff8f02866fce45c1d
Sebastian Wild <wild@liverpool.ac.uk>
2023-08-09 22:27:35 +0100

    proper generic script for pycon-energy

--- Experiments/pycon-energy.sh
+++ Experiments/pycon-energy.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-repetitions=100
+repetitions=1
+timestamp=`date +%Y-%m-%d_%H-%M-%S`
+host=`hostname`
 #echo `which python`
 algo=`which python`
 algo=${algo%/venv/bin/python}
-algo=${algo#/home/swild/Uni/projects/cpython-3.11-}
-outfile="PyConEnergy-${algo}-energy"
+algo=${algo#*cpython-3.11-}
+outfile="PyConEnergy-${algo}-energy-$host-$timestamp"
 echo "Running $algo for $repetitions repetitions, writing to $outfile."
 
-perf stat -o $outfile.data -r $repetitions -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/,power/energy-psys/ python PyConRunningTimes.py > $outfile
+perf stat -o $outfile.data -r $repetitions -e power/energy-cores/,power/energy-ram/,power/energy-gpu/,power/energy-pkg/ python PyConRunningTimes.py > $outfile
